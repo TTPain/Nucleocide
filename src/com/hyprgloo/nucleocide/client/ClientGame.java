84ebd9019fa0f04ab26cbcd344e9b0b99ad5c382
Zack Starrett <zas6@students.uwf.edu>
2021-02-13 22:30:44 -0600

    PacketPlayerStatus

--- src/com/hyprgloo/nucleocide/client/ClientGame.java
+++ src/com/hyprgloo/nucleocide/client/ClientGame.java
@@ -3,26 +3,28 @@ package com.hyprgloo.nucleocide.client;
 import com.hyprgloo.nucleocide.common.World;
 import com.hyprgloo.nucleocide.common.WorldGenerator;
 import com.hyprgloo.nucleocide.common.packet.PacketPlayerStatus;
+import com.osreboot.hvol2.direct.HvlDirect;
 import com.osreboot.ridhvl2.HvlCoord;
 
+/**
+ * @author basset
+ */
 public class ClientGame {
 
+	public static final String KEY_CLIENT = "game.playerstatus";
 	private World world;
 	private ClientPlayer player;
 	
 	public ClientGame(){
 		world = WorldGenerator.generate(""); // TODO get seed from lobby (os_reboot)
 		player = new ClientPlayer(new HvlCoord(), 1);
-		
-		//public PacketPlayerStatus = new PacketPlayerStatus(player.xPos, player.yPos);
-		
 		// TODO initialize the client game state here (basset)
 	}
 	
 	public void update(float delta){
 		// TODO update the client game / client networking here (basset)
-		
 		player.update(delta, world);
+		HvlDirect.writeUDP(KEY_CLIENT, new PacketPlayerStatus(player.playerPos, player.health));		
 		
 		// TODO update the client game / client networking here (basset)
 	}
