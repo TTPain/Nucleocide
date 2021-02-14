4f256ff1ca2084231830cbf00f5d837e24a8a244
Tristin <tristin.patane@gmail.com>
2021-02-13 22:46:48 -0600

    updated player movement

--- src/com/hyprgloo/nucleocide/client/ClientPlayer.java
+++ src/com/hyprgloo/nucleocide/client/ClientPlayer.java
@@ -1,12 +1,24 @@
 package com.hyprgloo.nucleocide.client;
 
+import static com.osreboot.ridhvl2.HvlStatics.hvlCirclec;
+import static com.osreboot.ridhvl2.HvlStatics.hvlDraw;
+
+import org.lwjgl.input.Keyboard;
+import org.newdawn.slick.Color;
+
 import com.hyprgloo.nucleocide.common.World;
 import com.osreboot.ridhvl2.HvlCoord;
+
+/*
+ * Author @TTPain
+ */
+
 
 public class ClientPlayer {
 	
 	public HvlCoord playerPos = new HvlCoord();
 	public float health;
+	private int pixPerSec = 40;
 	
 	
 
@@ -21,6 +33,21 @@ public class ClientPlayer {
 	public void update(float delta, World world){
 		// TODO implement player movement / collision here (tristin)
 		// TODO implement player shooting here (tristin)
+		
+		if(Keyboard.isKeyDown(Keyboard.KEY_W)) {
+			playerPos.y -= delta*pixPerSec;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_S)) {
+			playerPos.y += delta*pixPerSec;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_A)) {
+			playerPos.x -= delta*pixPerSec;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_D)) {
+			playerPos.x += delta*pixPerSec;
+		}
+		hvlDraw(hvlCirclec(playerPos.x,playerPos.y, 25, 25), Color.white);
+		System.out.println(playerPos);
+		
 	}
-	
 }
