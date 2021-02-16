3d0c33812ad0ff6e10ba3a57f15332d968e1d0cf
Tristin <tristin.patane@gmail.com>
2021-02-16 00:01:04 -0600

    removed bullet from ClientGame

--- src/com/hyprgloo/nucleocide/client/ClientGame.java
+++ src/com/hyprgloo/nucleocide/client/ClientGame.java
@@ -1,13 +1,11 @@
 package com.hyprgloo.nucleocide.client;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map.Entry;
 import java.util.Set;
 
+import com.hyprgloo.nucleocide.common.NetworkUtil;
 import com.hyprgloo.nucleocide.common.World;
 import com.hyprgloo.nucleocide.common.WorldGenerator;
+import com.hyprgloo.nucleocide.common.packet.PacketCollectivePlayerStatus;
 import com.hyprgloo.nucleocide.common.packet.PacketPlayerStatus;
 import com.osreboot.hvol2.direct.HvlDirect;
 import com.osreboot.ridhvl2.HvlCoord;
@@ -17,30 +15,36 @@ import com.osreboot.ridhvl2.HvlCoord;
  */
 public class ClientGame {
 	
-	public static final String KEY_CLIENT_STATUS = "game.playerstatus";
 	private World world;
 	private ClientPlayer player;
 	
-	
-	
-	
-	
-	
-	public ClientGame(){
+	public ClientGame(String id){
 		world = WorldGenerator.generate(""); // TODO get seed from lobby (os_reboot)
 		player = new ClientPlayerClient(new HvlCoord(), 1);
-		
 		// TODO initialize the client game state here (basset)
 	}
 	
-	public void update(float delta){
+	public void update(float delta, Set<String> lobbyPlayers){
 		// TODO update the client game / client networking here (basset)
 		player.update(delta, world);
-		HvlDirect.writeUDP(KEY_CLIENT_STATUS, new PacketPlayerStatus(player.playerPos, player.health));		
+		HvlDirect.writeUDP(NetworkUtil.KEY_PLAYER_STATUS, new PacketPlayerStatus(player.playerPos, player.health));		
 		// TODO update the client game / client networking here (basset)
 		
 		//Receive collective player status packet from server
-		//Use the information to render ClientPlayer objects for every other player, skipping the current client's info
+		PacketCollectivePlayerStatus packet;
+			if(HvlDirect.getKeys().contains(NetworkUtil.KEY_COLLECTIVE_PLAYER_STATUS)) {
+				packet = HvlDirect.getValue(NetworkUtil.KEY_COLLECTIVE_PLAYER_STATUS);
+				
+				//Detect how many players are connected via their usernames
+				//Use .getName() to compare to set of Strings
+				//Construct new ClientPlayers, update existing ones, remove ones as needed
+				
+				
+				//Use the information to render ClientPlayer objects for every other player, skipping the current client's info
+				for(int i = 0; i < packet.collectivePlayerStatus.size(); i++) {
+					
+				}
+			}
 	}
-	
 }
+
