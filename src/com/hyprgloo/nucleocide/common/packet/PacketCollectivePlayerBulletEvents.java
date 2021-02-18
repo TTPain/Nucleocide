6b38c165327fd71b78a2a15f6408ca7ebb1a02d6
Zack Starrett <zas6@students.uwf.edu>
2021-02-17 23:30:17 -0600

    Visiable aim indicators for other players

---
+++ src/com/hyprgloo/nucleocide/common/packet/PacketCollectivePlayerBulletEvents.java
@@ -0,0 +1,10 @@
+package com.hyprgloo.nucleocide.common.packet;
+
+import java.io.Serializable;
+
+public class PacketCollectivePlayerBulletEvents implements Serializable{
+	private static final long serialVersionUID = -4085980676197378754L;
+
+	
+	
+}
---
+++ src/com/hyprgloo/nucleocide/common/packet/PacketPlayerBulletEvents.java
@@ -0,0 +1,10 @@
+package com.hyprgloo.nucleocide.common.packet;
+
+import java.io.Serializable;
+
+public class PacketPlayerBulletEvents implements Serializable{
+	private static final long serialVersionUID = 3575486986403893346L;
+	
+	
+
+}
