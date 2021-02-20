5921cc6e2fd8e02ed3836ebf9346dd5ccbc7aaed
Zack Starrett <zas6@students.uwf.edu>
2021-02-20 01:43:11 -0600

    ServerEnemy

--- src/com/hyprgloo/nucleocide/server/ServerEnemy.java
+++ src/com/hyprgloo/nucleocide/server/ServerEnemy.java
@@ -1,9 +1,22 @@
 package com.hyprgloo.nucleocide.server;
 
 import com.hyprgloo.nucleocide.common.World;
+import com.osreboot.ridhvl2.HvlCoord;
 
-public abstract class ServerEnemy {
+public class ServerEnemy {
 	
-	public abstract void update(float delta, World world); // TODO extend this class and implement basic AI (???)
+	public HvlCoord enemyPos = new HvlCoord();
+	public float health;
+	public int textureID;
+	public int pathfindingID;
+	
+	public ServerEnemy(HvlCoord enemyPosArg, float healthArg, int textureIDArg, int pathfindingIDArg) {
+		enemyPos = enemyPosArg;
+		health = healthArg;
+		textureID = textureIDArg;
+		pathfindingID = pathfindingIDArg;
+	}
+	
+	public void update(float delta, World world) {} // TODO extend this class and implement basic AI (???)
 
 }
